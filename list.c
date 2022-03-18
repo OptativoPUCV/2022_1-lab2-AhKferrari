fbd41d40416d21480fcca2a7ed564fce264e13f7
ahkferrari <>
2022-03-18 15:02:28 +0000

    new try

--- list.c
+++ list.c
@@ -43,7 +43,7 @@ void * firstList(List * list) {
 }
 
 void * nextList(List * list) {
-  if(!list->current || list->next) return NULL;
+  if(!list->current || list->current->next) return NULL;
   list->current->next;
   return list->current->data;
 }

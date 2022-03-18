fb9e0d291b24ecab01d66a9799cf0f42715ed56f
ahkferrari <>
2022-03-18 14:41:58 +0000

    new try

--- list.c
+++ list.c
@@ -30,9 +30,9 @@ Node * createNode(void * data) {
 
 List * createList() {
   List * nueva = (List *)malloc(sizeof(List));
-  nueva->head = NULL;
-  nueva->current = NULL;
-  nueva->tail = NULL;
+  nueva->head;
+  nueva->current;
+  nueva->tail;
   return NULL;
 }
 
